9e378bbf9ffae13a28c866c6766d54c3c5145038
Steven Decker <swdecker@gmail.com>
2019-09-11 17:10:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,11 +16,12 @@ end
 
 def prompt_user
   # code #prompt_user here
-  puts
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets
 end
 
 def end_game
